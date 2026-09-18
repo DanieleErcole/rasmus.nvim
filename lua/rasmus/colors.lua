0a10fa60b81a3ee16738f3f2c6d5767cec68cc32
DanieleErcole <danieleercole14@protonmail.com>
2026-09-18 13:00:55 +0200

    oxocarbon color theme applied to rasmus

--- lua/rasmus/colors.lua
+++ lua/rasmus/colors.lua
@@ -1,71 +1,37 @@
 local M = {}
 
 M.dark = {
-  -- Basic
-  bg = "#1a1a19",
-  fg = "#d1d1d1",
-  -- Normal
-  black = "#333332",
-  red = "#ff968c",
-  green = "#61957f",
-  yellow = "#ffc591",
-  blue = "#8db4d4",
-  magenta = "#de9bc8",
-  cyan = "#7bb099",
-  white = "#d1d1d1",
-  -- Bright
-  bright_black = "#4c4c4b",
-  bright_red = "#ffafa5",
-  bright_green = "#7aae98",
-  bright_yellow = "#ffdeaa",
-  bright_blue = "#a6cded",
-  bright_magenta = "#f7b4e1",
-  bright_cyan = "#94c9b2",
-  bright_white = "#eaeaea",
-  -- Grays
-  gray01 = "#222221",
-  gray02 = "#2a2a29",
-  gray03 = "#323231",
-  gray04 = "#3a3a39",
-  gray05 = "#6a6a69",
-  gray06 = "#767675",
-  gray07 = "#b6b6b5",
-  -- Special
-  none = "NONE",
-}
-
-M.monochrome = {
-  -- Basic
-  bg = "#1a1a19",
-  fg = "#d1d1d1",
-  -- Normal
-  black = "#333332",
-  red = "#c5c5c5",
-  green = "#D0D0D0",
-  yellow = "#DEDEDE",
-  blue = "#B3B3B3",
-  magenta = "#bcbcbc",
-  cyan = "#B3B3B3",
-  white = "#d1d1d1",
-  -- Bright
-  bright_black = "#4c4c4b",
-  bright_red = "#d2d2d2",
-  bright_green = "#e9e9e9",
-  bright_yellow = "#f7f7f7",
-  bright_blue = "#cccccc",
-  bright_magenta = "#d5d5d5",
-  bright_cyan = "#cccccc",
-  bright_white = "#eaeaea",
-  -- Grays
-  gray01 = "#222221",
-  gray02 = "#2a2a29",
-  gray03 = "#323231",
-  gray04 = "#3a3a39",
-  gray05 = "#6a6a69",
-  gray06 = "#767675",
-  gray07 = "#b6b6b5",
-  -- Special
-  none = "NONE",
+    -- Basic
+    bg = "#161616",
+    fg = "#dde1e6",
+    -- Normal
+    black = "#262626",
+    red = "#ff7eb6",
+    green = "#42be65",
+    yellow = "#82cfff",
+    blue = "#33b1ff",
+    magenta = "#ee5396",
+    cyan = "#3ddbd9",
+    white = "#dde1e6",
+    -- Bright
+    bright_black = "#393939",
+    bright_red = "#ff7eb6",
+    bright_green = "#32be65",
+    bright_yellow = "#82cfff",
+    bright_blue = "#33b1ff",
+    bright_magenta = "#ee5396",
+    bright_cyan = "#3ddbd9",
+    bright_white = "#ffffff",
+    -- Grays
+    gray01 = "#222222",
+    gray02 = "#2a2a2a",
+    gray03 = "#323232",
+    gray04 = "#3a3a3a",
+    gray05 = "#6a6a6a",
+    gray06 = "#767676",
+    gray07 = "#b6b6b6",
+    -- Special
+    none = "NONE",
 }
 
 return M
