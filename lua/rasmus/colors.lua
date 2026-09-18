723d454213465f03c6fc8b0272040e86518f9d73
DanieleErcole <danieleercole14@protonmail.com>
2026-09-18 13:08:26 +0200

    cg colors

--- lua/rasmus/colors.lua
+++ lua/rasmus/colors.lua
@@ -2,36 +2,37 @@ local M = {}
 
 M.dark = {
     -- Basic
-    bg = "#161616",
-    fg = "#dde1e6",
+    bg = "#1a1a19",
+    fg = "#d1d1d1",
     -- Normal
-    black = "#262626",
-    red = "#ff7eb6",
-    green = "#33b1ff",
-    yellow = "#82cfff",
-    blue = "#42be65",
-    magenta = "#ee5396",
-    cyan = "#3ddbd9",
-    white = "#dde1e6",
+    black = "#333332",
+    red = "#ff968c",
+    green = "#61957f",
+    yellow = "#ffc591",
+    blue = "#8db4d4",
+    magenta = "#de9bc8",
+    cyan = "#7bb099",
+    white = "#d1d1d1",
     -- Bright
-    bright_black = "#393939",
-    bright_red = "#ff7eb6",
-    bright_green = "#33b1ff",
-    bright_yellow = "#82cfff",
-    bright_blue = "#52ce65",
-    bright_magenta = "#ee5396",
-    bright_cyan = "#3ddbd9",
-    bright_white = "#ffffff",
+    bright_black = "#4c4c4b",
+    bright_red = "#ffafa5",
+    bright_green = "#7aae98",
+    bright_yellow = "#ffdeaa",
+    bright_blue = "#a6cded",
+    bright_magenta = "#f7b4e1",
+    bright_cyan = "#94c9b2",
+    bright_white = "#eaeaea",
     -- Grays
-    gray01 = "#222222",
-    gray02 = "#2a2a2a",
-    gray03 = "#323232",
-    gray04 = "#3a3a3a",
-    gray05 = "#6a6a6a",
-    gray06 = "#767676",
-    gray07 = "#b6b6b6",
+    gray01 = "#222221",
+    gray02 = "#2a2a29",
+    gray03 = "#323231",
+    gray04 = "#3a3a39",
+    gray05 = "#6a6a69",
+    gray06 = "#767675",
+    gray07 = "#b6b6b5",
     -- Special
     none = "NONE",
 }
+
 
 return M
